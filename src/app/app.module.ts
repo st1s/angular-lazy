7ce62a18ca6241e3e1d5483e75e0b53b358fbd5d
Stewan <oi@stewan.io>
2017-08-02 17:06:21 -0300

    chore()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 /**
  * Created by NamitaMalik on 9/27/2016. Adapted by Stewan Pacheco on 02/08/2017.
  */
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ProviderService } from "./provider.service";
 
 @NgModule({
